0f1758052eb49ea09f89ab21c87eae0b10b40aa1
flyingbutton <thedrafttin@gmail.com>
2025-01-03 12:32:20 -0500

    Add Navbar to all pages

--- personal-blog-frontend/src/App.jsx
+++ personal-blog-frontend/src/App.jsx
@@ -1,16 +1,20 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import BlogDetails from "./pages/BlogDetails";
 import BlogEditor from "./pages/BlogEditor";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/blogs/:id" element={<BlogDetails />} />
-        <Route path="/editor" element={<BlogEditor />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blogs/:id" element={<BlogDetails />} />
+          <Route path="/editor" element={<BlogEditor />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 }
