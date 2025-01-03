a5f03e9e72a27c48c157b3bcba3cce17ba6452cd
flyingbutton <thedrafttin@gmail.com>
2025-01-03 12:29:46 -0500

    Make category field non-essential

--- personal-blog-backend/models/Blog.js
+++ personal-blog-backend/models/Blog.js
@@ -5,7 +5,7 @@ const BlogSchema = new mongoose.Schema(
     title: { type: String, required: true },
     content: { type: String, required: true },
     author: { type: String, default: "Anonymous" },
-    category: { type: String, required: true },
+    category: { type: String, default: "Others" },
   },
   // createdAt & updatedAt fields
   { timestamps: true }
