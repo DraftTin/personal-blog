29930957046ed172653fa8a2039d3841a9f775de
flyingbutton <thedrafttin@gmail.com>
2025-02-05 10:46:40 -0500

    Remove old code

--- personal-blog-backend/server.js
+++ personal-blog-backend/server.js
@@ -36,22 +36,6 @@ app.get("/", (req, res) => {
   res.send("Welcome to the Personal Blog Backend!");
 });
 
-app.post("/api/blogs", (req, res) => {
-  const newBlog = req.body;
-  const hasAllKeys = ["title", "content"].every((key) => key in req.body);
-  if (hasAllKeys == true) {
-    blogs.push({ id: id, title: req.body.title, content: req.body.content });
-    res
-      .status(201)
-      .json({ message: "okay, you have gotten it", blog: newBlog });
-  } else {
-    res.status(400).json({
-      error: "Invalid JSON format",
-      message: "Should contain 'title' and 'content' fields",
-    });
-  }
-});
-
 // Start the server
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
